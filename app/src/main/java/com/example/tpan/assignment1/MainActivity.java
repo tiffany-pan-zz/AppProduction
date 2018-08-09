53653b56b31834d58ec7c0028c0058642242eb23
Tiffany Pan <t1pan@ucsd.edu>
2018-08-08 21:40:10 -0700

    Added Ad Banner

--- app/src/main/java/com/example/tpan/assignment1/MainActivity.java
+++ app/src/main/java/com/example/tpan/assignment1/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.tpan.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-2786037823422025~8844755781");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
